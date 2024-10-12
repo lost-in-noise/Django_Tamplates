bbefdbb4271fbf9359dd839225f010b0c8ae725a
lost-in-noise <nancykikvadze@gmail.com>
2024-10-13 00:08:54 +0400

    Create a View to Render the Template

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_detail(request):
+    post = {
+        'title': 'My First Blog Post',
+        'content': 'This is the content of the blog post.',
+        'date': '2024-10-12',
+        'author': 'Nancy',
+        'comments': [
+            {'user': 'John', 'text': 'Great post!'},
+            {'user': 'Alice', 'text': 'Thanks for sharing!'}
+        ]
+    }
+    return render(request, 'myapp/post.html', {'post': post})
